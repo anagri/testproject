1fa33b0425cb2902ee22cf6e1aa16fbff724fcb3
Amiruddin Nagri <amir.nagri@gmail.com>
2010-08-30 13:15:14 +0530

    [Amir] rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testproject::Application.config.secret_token = 'a93c9f5dde59bd698e77f88a70e7249cab2801c923d6ec6f88baf47595e80c84c924d049d096721458102b1af15d2fa3c6f22cd90f86cfbde4918cf8400809b2'
